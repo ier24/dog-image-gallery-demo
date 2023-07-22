2b77203fd8f00517310654fdf3bb322067b70090
ier24 <ryohr93@gmail.com>
2023-07-22 17:16:05 +0900

    feat: 特定の犬種の画像をランダムに表示する

---
+++ src/api.ts
@@ -0,0 +1,7 @@
+export async function fetchDogImages(breed: string) {
+  const response = await fetch(
+    `https://dog.ceo/api/breed/${breed}/images/random/12`
+  );
+  const data = await response.json();
+  return data.message;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import "bulma/css/bulma.css";
+import { createRoot } from "react-dom/client";
 import React from "react";
 import App from "./App";
-import { createRoot } from "react-dom/client";
 
 // createRoot()の引数としてnullは許容されないので、非nullアサーション演算子を使っておく（やめたい）
 createRoot(document.querySelector("#content")!).render(<App />);
